eb6334e561c55d5bad1b06bc21684d338190207d
Vivek Shukla <vivek.f19@gmail.com>
2015-01-27 14:51:32 -0500

    Added contact to the contacts array. Also redirected the page to /contacts in order display contacts.

--- crm.rb
+++ crm.rb
@@ -11,15 +11,15 @@ get '/' do
 end
 
 get '/contacts' do
-    @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
-
   erb :contacts
   end
 
 get '/contacts/new' do
-  erb :new
+  erb :new_contact
+end
+
+post '/contacts' do
+  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  $rolodex.add_contact(new_contact)
+  redirect to('/contacts')
 end
