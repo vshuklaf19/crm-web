6134f09a4c7f5c07e20f3d8276cdf45ae6bd5e96
Vivek Shukla <vivek.f19@gmail.com>
2015-01-27 14:39:39 -0500

    Added the Rolodex class in order to deal with a list of contacts.

--- crm.rb
+++ crm.rb
@@ -1,12 +1,22 @@
-require '/contact'
+require './rolodex'
+require './contact'
 require 'sinatra'
 
+$rolodex= Rolodex.new
+
+#routes
 get '/' do
   @crm_app_name = "My CRM"
   erb :index 
 end
 
 get '/contacts' do
+    @contacts = []
+  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+
+
   erb :contacts
   end
 
