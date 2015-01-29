f4c709f6250ee590f57edcb35dc51a38ebb2a66f
Vivek Shukla <vivek.f19@gmail.com>
2015-01-29 13:53:18 -0500

    Created a fake contact to check functionality.

--- crm.rb
+++ crm.rb
@@ -3,7 +3,10 @@ require './contact'
 require 'sinatra'
 require 'sinatra/reloader'
 
-$rolodex= Rolodex.new
+@@rolodex= Rolodex.new
+
+# Temporary fake data so that we always find contact with id 1000.
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
 
 #routes
 get '/' do
@@ -26,4 +29,9 @@ post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
-end
+end 
+
+get '/contacts/1000' do
+  @contact = @@rolodex.find(1000)
+  erb :show_contact
+end  
--- rolodex.rb
+++ rolodex.rb
@@ -6,9 +6,15 @@ class Rolodex
     @id = 1000
   end
 
+  @@rolodex = Rolodex.new
+
   def add_contact(contact)
     contact.id = @id
     @contacts << contact
     @id += 1
+  end
+
+  def find(contact_id)
+     @contacts.find {|contact| contact.id == contact_id }
   end
 end
