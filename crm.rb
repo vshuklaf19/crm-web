1f81f392ee219179ef6ddc487bd8ea314968d308
Vivek Shukla <vivek.f19@gmail.com>
2015-01-29 14:59:45 -0500

    Removed fake contacts and checked all functionality.

--- crm.rb
+++ crm.rb
@@ -5,12 +5,6 @@ require 'sinatra/reloader'
 
 @@rolodex= Rolodex.new
 
-# Temporary fake data so that we always find contact with id 1000.
-@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar"))
-@@rolodex.add_contact(Contact.new("Tom", "Jones", "tjones@bitmakerlabs.com", "Jerry's enemy"))
-@@rolodex.add_contact(Contact.new("Rick", "Ross", "rross@bitmakerlabs.com", "Fat fuck!"))
-@@rolodex.add_contact(Contact.new("Billy", "Smith", "bsmith@bitmakerlabs.com", "Average Joe"))
-
 #routes
 get '/' do
   @title = "Main Page"
@@ -30,7 +24,7 @@ end
 
 post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
-  $rolodex.add_contact(new_contact)
+  @@rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end 
 
