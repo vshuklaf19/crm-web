9d05d145ac5c2d19ae363cd1cef457e742e3f86b
Vivek Shukla <vivek.f19@gmail.com>
2015-01-29 14:29:00 -0500

    Added a Edit contact feature.

--- crm.rb
+++ crm.rb
@@ -37,8 +37,17 @@ end
 get '/contacts/:id' do
   @contact = @@rolodex.find(params[:id].to_i)
   if @contact
-  erb :show_contact
-else
-  raise Sinatra::NotFound
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
-end  
+
+get "/contacts/:id/edit" do
+  @contact = @@rolodex.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end 
